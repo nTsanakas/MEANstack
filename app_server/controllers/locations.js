6bc86249088bed4408a514ebfbdc62dbd1ec4ff8
Nikolaos Tsanakas <n.tsanakas@ymail.com>
2017-04-10 15:49:29 +0200

    static site and image

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 /* GET home page */
 module.exports.homelist = function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('locations-list', { title: 'Home'});
 };
 
 /* GET location info page */
 module.exports.locationInfo = function(req, res, next) {
-  res.render('index', { title: 'Location Info'});
+  res.render('location-info', { title: 'Location Info'});
 };
 
 /* GET Add review page */
